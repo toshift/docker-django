4271f6a5e44ef1533a6083ff6a98548344562d99
yamauchi toshikazu <yamauchi@JIG-SAW.COM>
2016-10-03 15:26:08 +0900

    test add viewの汎用化

--- app/common/apps/webmain/views.py
+++ app/common/apps/webmain/views.py
@@ -3,28 +3,27 @@ from django.http import HttpResponse ,HttpResponseRedirect### httpresponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'webmain/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'webmain/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-#  ### http404 
-#    try:
-#        question = Question.objects.get(pk=question_id)
-#    except Question.DoesNotExist:
-#        raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'webmain/detail.html', {'question':question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-### httpresponse    return HttpResponse(response % question_id)
-    return render(request, 'webmain/results.html', {'question':question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'webmain/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'webmain/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
